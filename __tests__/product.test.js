662428c4f39bf5a6d830790cc3bbacc31b6d25a2
JoLeaper <joleap123@gmail.com>
2020-06-09 14:31:03 -0700

    patch route test written

--- __tests__/product.test.js
+++ __tests__/product.test.js
@@ -84,9 +84,35 @@ describe('pos-bar-backend routes', () => {
             });
     });
 
-    // it('updates information about the product', () => {
+    
 
-    // });
+    it('updates information about the product', () => {
+        it('gets a specific product by id with details', async() => {
+            return Product.create({
+                name: 'Captain Morgan Spiced Rum',
+                description: 'US Virgin Islands- Mixes aromas of marshmallow, light toffee and light spiced honey, leading into a molasses-centric flavor. Ideal for spicing up tropical cocktails or mixed with cola.',
+                salePricePerMl: 0.02,
+                purchasePricePerBottle: 14.99,
+                size: 750,
+            })
+                .then(product => {
+                    return request(app)
+                        .patch(`/api/v1/products/${product._id}`)
+                        .send({ name: 'Captain Morgan\'s Super Special Spiced Rum' });
+                })
+                .then(res => {
+                    expect(res.body).toEqual({
+                        _id: expect.anything(),
+                        name: 'Captain Morgan\'s Super Special Spiced Rum',
+                        description: 'US Virgin Islands- Mixes aromas of marshmallow, light toffee and light spiced honey, leading into a molasses-centric flavor. Ideal for spicing up tropical cocktails or mixed with cola.',
+                        salePricePerMl: 0.02,
+                        purchasePricePerBottle: 14.99,
+                        size: 750,
+                        __v: 0
+                    });
+                });
+        });
+    });
 
     // it('deletes a product in case it is discontinued', () => {
 
