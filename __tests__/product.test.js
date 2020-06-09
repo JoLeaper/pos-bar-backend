4f2b786eca6233ab551f58dd66b5f8cf0821a60e
JoLeaper <joleap123@gmail.com>
2020-06-09 14:09:34 -0700

    get all test route written

--- __tests__/product.test.js
+++ __tests__/product.test.js
@@ -45,9 +45,22 @@ describe('pos-bar-backend routes', () => {
             });
     });
 
-    // it('gets all the products we offer (id and name only)', () => {
-
-    // });
+    it('gets all the products we offer (id and name only)', () => {
+        return Product.create({
+            name: 'Captain Morgan Spiced Rum',
+            description: 'US Virgin Islands- Mixes aromas of marshmallow, light toffee and light spiced honey, leading into a molasses-centric flavor. Ideal for spicing up tropical cocktails or mixed with cola.',
+            salePricePerMl: 0.02,
+            purchasePricePerBottle: 14.99,
+            size: 750,
+        })
+            .then(() => request(app.get('api/v1/products')))
+            .then(res => {
+                expect(res.body).toEqual([{
+                    id: expect.anything(),
+                    name: 'Captain Morgan Spiced Rum'
+                }]);
+            });
+    });
 
     // it('gets a specific product by id with details', () => {
 
