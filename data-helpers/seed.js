8a99bc769581923a3ce2c3386cea3e0a7c007b09
JoLeaper <joleap123@gmail.com>
2020-06-24 14:27:07 -0700

    data successfully seeded

--- data-helpers/seed.js
+++ data-helpers/seed.js
@@ -7,22 +7,22 @@ module.exports = async({ products = 10, bottles = 50, orders = 20  } = {}) => {
   const productArray = await Promise.all([...Array(products)].map(async() => {
     return Product.create({
       name: chance.profession() + ' ' + chance.last() + ' Drink',  
-      description: chance.paragraph(),
+      description: chance.paragraph({ sentences: 1 }),
       salePricePerMl: chance.floating({ fixed: 2 }),
       purchasePricePerBottle: chance.floating({ fixed: 2 }),
       size: chance.natural({ min: 1 })
     });
   }));
 
-  await Promise.all([...Array(bottles)]).map(async() => {
-    const product = productArray.pickone();
+  await Promise.all([...Array(bottles)].map(async() => {
+    const product = chance.pickone(productArray);
     return Bottle.create({
       product: product.id,
       remainingLiquid: chance.natural({ min: 1, max: product.size }),
       purchaseDate: chance.date(),
       lastPourDate: chance.date()
     });
-  });
+  }));
 
   await Promise.all([...Array(orders)].map(async() => {
     const amount = chance.natural({ min: 1, max: 5 });
